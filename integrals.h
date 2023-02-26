f49fbe9dc0f293822f65e59007ff9c2df196b6ce
nazar <xfghbdgh@gmail.com>
2023-02-26 03:49:20 +0200

    Added integration tools

--- integrals.h
+++ integrals.h
@@ -5,4 +5,69 @@
 #ifndef INTEGRATE_SERIAL_INTEGRALS_H
 #define INTEGRATE_SERIAL_INTEGRALS_H
 
+#include <cstddef>
+#include <cmath>
+
+namespace integrals {
+
+    /* Calculates the integral of the function with given relative and
+     * absolute errors. The function must be of two parameters.
+     * */
+
+    template <class F>
+    double calculate_riemann_sum(F function, double x_start, double x_end, double y_start,
+                                 double y_end, size_t steps_x, size_t steps_y,
+                                 double shift_x = 0, double shift_y = 0) {
+        auto delta_x = (x_end - x_start) / (double)steps_x;
+        auto delta_y = (y_end - y_start) / (double)steps_y;
+        double result = 0;
+        for (size_t step_x = 0; step_x != steps_x; step_x++) {
+            for (size_t step_y = 0; step_y != steps_y; step_y++) {
+                auto value_of_func = function(x_start + (double)step_x * delta_x + shift_x,
+                                              y_start + (double)step_y * delta_y + shift_y);
+                result += delta_x * delta_y * value_of_func;
+            }
+        }
+        return result;
+    }
+
+    template <class F>
+    std::tuple<double, double, double> calculate_integral(F function, double abs_err, double rel_err,
+                              double x_start, double x_end, double y_start,
+                              double y_end, size_t init_steps_x,
+                              size_t init_steps_y, size_t max_iter) {
+
+        // first step: dividing an interval and computing
+        // the initial value of the riemann sum over it
+        double first_riemann_sum = 0;
+        double second_riemann_sum = 0;
+        auto steps_x = init_steps_x;
+        auto steps_y = init_steps_y;
+        size_t counter = 0;
+        do {
+            auto delta_x = (x_end - x_start) / (double)steps_x;
+            auto delta_y = (y_end - y_start) / (double)steps_y;
+
+            if (counter == 0) {
+                first_riemann_sum = calculate_riemann_sum(function, x_start, x_end, y_start, y_end, steps_x, steps_y);
+            }
+            else {
+                first_riemann_sum = second_riemann_sum;
+            }
+            second_riemann_sum = first_riemann_sum / 2 + 0.5 *
+                    calculate_riemann_sum(function, x_start, x_end, y_start, y_end,
+                                          steps_x - 1, steps_y - 1, delta_x / 2, delta_y / 2);
+            steps_x *= 2;
+            steps_y *= 2;
+        }
+        while (fabs(second_riemann_sum - first_riemann_sum) < abs_err &&
+                fabs((second_riemann_sum - first_riemann_sum) / second_riemann_sum) < rel_err ||
+                counter++ != max_iter);
+
+        return std::make_tuple(second_riemann_sum, fabs(second_riemann_sum - first_riemann_sum),
+                               fabs((second_riemann_sum - first_riemann_sum) / second_riemann_sum));
+    }
+}
+
+
 #endif //INTEGRATE_SERIAL_INTEGRALS_H
--- main.cpp
+++ main.cpp
@@ -6,13 +6,30 @@
 #include <numbers>
 #include <fstream>
 #include <sstream>
+#include <chrono>
+#include <atomic>
+#include <array>
+#include "integrals.h"
 
+namespace time_functions {
+    inline std::chrono::high_resolution_clock::time_point get_current_time_fenced() {
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+        auto res_time = std::chrono::high_resolution_clock::now();
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+        return res_time;
+    }
+
+    template<class D>
+    inline long long to_ms(const D &d) {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(d).count();
+    }
+}
 
 namespace functions {
     double f1(double x1, double x2) {
         double sum = 0.0;
-        for (int i = -2; i != 2; i++) {
-            for (int j = -2; j != 2; j++) {
+        for (int i = -2; i <= 2; i++) {
+            for (int j = -2; j <= 2; j++) {
                 sum += 1 / (5 * (i + 2) + j + 3 + pow((x1 - 16 * j), 6) + pow((x2 - 16 * i), 6));
             }
         }
@@ -20,11 +37,39 @@ namespace functions {
         return 1 / sum;
     }
 
-    double f2(double x1, double x2, double a = 20, double b = 0.2, double c = 2 * std::numbers::pi) {
-
-        return {};
+    double f2(double x1, double x2) {
+        double a = 20;
+        double b = 0.2;
+        double c = 2 * std::numbers::pi;
+        auto first_term = -a * exp(-b * sqrt(0.5 * (x1 * x1 + x2 * x2)));
+        auto second_term = -exp(0.5 * (cos(c * x1) + cos(c * x2)));
+        return first_term + second_term + a + exp(1);
     }
 
+    double f3(double x1, double x2) {
+        size_t m = 5;
+        double a1[]{1, 2, 1, 1, 5};
+        double a2[]{4, 5, 1, 2, 4};
+        double c[]{2, 1, 4, 7, 2};
+
+        double sum = 0;
+        for (size_t i = 0; i != m; i++) {
+            auto pattern = (x1 - a1[i]) * (x1 - a1[i]) + (x2 - a2[i]) * (x2 - a2[i]);
+            auto first_factor = c[i] * exp(-1 / std::numbers::pi * pattern);
+            auto second_factor = cos(std::numbers::pi * pattern);
+            sum += first_factor * second_factor;
+        }
+        return -sum;
+    }
+
+    double f4(double x1, double x2) {
+        double a = 20;
+        double b = 0.2;
+        double c = 2 * std::numbers::pi;
+        auto first_term = -a * exp(-b * sqrt(0.5 * (x1 * x1, x2 * x2)));
+        auto second_term = -exp(0.5 * (cos(c * x1) + cos(c * x2)));
+        return first_term + second_term + a + exp(1);
+    }
 
 }
 
@@ -33,7 +78,7 @@ std::vector<std::string> file_reader(std::ifstream &filestream) {
     std::vector<std::string> result;
     std::string word;
     while (std::getline(filestream, word)) {
-        if (word.empty() || (word.find('=') == word.size())) {
+        if (word.empty() || word.find('=') == word.size()) {
             continue;
         }
         result.push_back(word);
@@ -44,7 +89,7 @@ std::vector<std::string> file_reader(std::ifstream &filestream) {
 // gets configuration parameters from file given
 std::map<std::string, double> get_config(const std::string &file_name) {
     std::ifstream config_file(file_name);
-    if (!config_file.is_open()){
+    if (!config_file.is_open()) {
         throw std::system_error();
     }
     // reading the content
@@ -52,18 +97,18 @@ std::map<std::string, double> get_config(const std::string &file_name) {
 
     // making the map of default config properties
     std::map<std::string, double> result = {
-            {"abs_err",  0.000005},
-            {"rel_err",  0.0002},
-            {"x_start", -50},
-            {"x_end", 50},
-            {"y_start", -50},
-            {"y_end", 50},
+            {"abs_err",      0.000005},
+            {"rel_err",      0.0002},
+            {"x_start",      -50},
+            {"x_end",        50},
+            {"y_start",      -50},
+            {"y_end",        50},
             {"init_steps_x", 100},
             {"init_steps_y", 100},
-            {"max_iter", 20}
+            {"max_iter",     20}
     };
 
-    for (auto& expr: expressions) {
+    for (auto &expr: expressions) {
         if (expr[0] == '#') {
             continue;
         }
@@ -98,7 +143,7 @@ std::map<std::string, double> get_config(const std::string &file_name) {
         try {
             value_parsed = std::stoi(value);
         }
-        catch (std::invalid_argument&) {
+        catch (std::invalid_argument &) {
             std::string error_message = std::string("Invalid value ");
             error_message.append(value);
             error_message.append(" for parameter ");
@@ -165,6 +210,29 @@ int main(int argc, char **argv) {
         std::cerr << "Invalid function number" << std::endl;
         exit(2);
     }
+
+    const std::function<double(double, double)> functions[] = {
+            functions::f1,
+            functions::f2,
+            functions::f3,
+            functions::f4,
+    };
+
+    auto start_time_point = time_functions::get_current_time_fenced();
+
+    auto res = integrals::calculate_integral(functions[num - 1], config["abs_err"], config["rel_err"], config["x_start"],
+                                             config["x_end"],
+                                             config["y_start"], config["y_end"], (size_t) config["init_steps_x"],
+                                             (size_t) config["init_steps_y"], (size_t) config["max_iter"]);
+
+    auto end_time_point = time_functions::get_current_time_fenced();
+    auto dur = end_time_point - start_time_point;
+
+
+    std::cout << std::get<0>(res) << std::endl;
+    std::cout << std::get<1>(res) << std::endl;
+    std::cout << std::get<2>(res) << std::endl;
+    std::cout << time_functions::to_ms(dur) << std::endl;
 
     return 0;
 }
