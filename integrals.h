040a0fed1d63507f63d91be4d03e46ce924fee40
nazar <xfghbdgh@gmail.com>
2023-02-27 21:20:05 +0200

    aaaaa

--- integrals.h
+++ integrals.h
@@ -10,10 +10,10 @@
 
 namespace integrals {
 
-    /* Calculates the riemann sum of the function with given relative and
-     * absolute errors, divided by delta_x * delta_y.
-     * The function must be of two parameters.
+    /* Calculates the integral of the function with given relative and
+     * absolute errors. The function must be of two parameters.
      * */
+
     template <class F>
     double calculate_riemann_sum(F function, double x_start, double x_end, double y_start,
                                  double y_end, size_t steps_x, size_t steps_y,
@@ -25,7 +25,7 @@ namespace integrals {
             for (size_t step_y = 0; step_y != steps_y; step_y++) {
                 auto value_of_func = function(x_start + (double)step_x * delta_x + shift_x,
                                               y_start + (double)step_y * delta_y + shift_y);
-                result += value_of_func;
+                result += delta_x * delta_y * value_of_func;
             }
         }
         return result;
@@ -33,9 +33,9 @@ namespace integrals {
 
     template <class F>
     std::tuple<double, double, double> calculate_integral(F function, double abs_err, double rel_err,
-                                                          double x_start, double x_end, double y_start,
-                                                          double y_end, size_t init_steps_x,
-                                                          size_t init_steps_y, size_t max_iter) {
+                              double x_start, double x_end, double y_start,
+                              double y_end, size_t init_steps_x,
+                              size_t init_steps_y, size_t max_iter) {
 
         // first step: dividing an interval and computing
         // the initial value of the riemann sum over it
@@ -45,11 +45,14 @@ namespace integrals {
         auto steps_y = init_steps_y;
         size_t counter = 0;
         do {
+            if (counter > max_iter) {
+                break;
+            }
             auto delta_x = (x_end - x_start) / (double)steps_x;
             auto delta_y = (y_end - y_start) / (double)steps_y;
 
             if (counter == 0) {
-                first_riemann_sum = delta_x * delta_y * calculate_riemann_sum(function, x_start, x_end, y_start, y_end, steps_x, steps_y);
+                first_riemann_sum = calculate_riemann_sum(function, x_start, x_end, y_start, y_end, steps_x, steps_y);
             }
             else {
                 first_riemann_sum = second_riemann_sum;
@@ -61,13 +64,12 @@ namespace integrals {
                                                 steps_x, steps_y, 0, delta_y / 2);
             auto third = calculate_riemann_sum(function, x_start, x_end, y_start, y_end,
                                                steps_x, steps_y, delta_x / 2, delta_y / 2);
-            second_riemann_sum = 0.25 * (first_riemann_sum + delta_x * delta_y * (first + second + third));
+            second_riemann_sum = 0.25 * (first_riemann_sum + first + second + third);
             steps_x *= 2;
             steps_y *= 2;
         }
         while (fabs(second_riemann_sum - first_riemann_sum) < abs_err &&
-               fabs((second_riemann_sum - first_riemann_sum) / second_riemann_sum) < rel_err ||
-               counter++ != max_iter);
+                fabs((second_riemann_sum - first_riemann_sum) / second_riemann_sum) < rel_err);
 
         return std::make_tuple(second_riemann_sum, fabs(second_riemann_sum - first_riemann_sum),
                                fabs((second_riemann_sum - first_riemann_sum) / second_riemann_sum));
