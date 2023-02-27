c775db5ead6256635f9013978c079c026f2a08f7
nazar <xfghbdgh@gmail.com>
2023-02-27 19:05:39 +0200

    Added shell script

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ from itertools import combinations
 
 
 def run_executable(func):
-    cmd = ["./integrate_serial", f"func{func}.cfg", str(func)]
+    cmd = ["./build/integrate_serial", f"../func{func}.cfg", str(func)]
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout = result.stdout.decode("utf-8")
     exit_code = result.returncode
@@ -24,11 +24,11 @@ def analyze(repetitions):
             integrals[i], abs_err, rel_err, times[i] = list(map(float, run_executable(func).strip().split("\n")))
 
         if all(abs(i - j) < 10 ** (-7) for i, j in combinations(integrals, 2)):
-            results += [integrals[0], "\n", abs_err, "\n", rel_err, "\n", min(times), "\n",
-                        int(sum(times) / repetitions), "\n", int(scipy.stats.tstd(times)), "\n"]
+            results += [f"{integrals[0]}\n{abs_err}\n{rel_err}\n{int(min(times))}\n" +
+                        f"{int(sum(times) / repetitions)}\n{int(scipy.stats.tstd(times)) if repetitions > 1 else 0}"]
         else:
             results += ["Different values of integers.", "\n"]
-    print(*results)
+    print("\n\n".join(results))
 
 
 def parser():
